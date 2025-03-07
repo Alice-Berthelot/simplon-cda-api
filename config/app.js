5829833de18fc0540ee700f127cb96e778c85ff6
ALICE BERTHELOT <aliceberthelot.pro@gmail.com>
2025-03-07 21:27:33 +0100

    modified link to front, tried getrandomjoke

--- config/app.js
+++ config/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const jokeRoutes = require('../routes/joke')
+require('dotenv').config();
 
 app.use(express.json()); 
 
--- controllers/jokeController.js
+++ controllers/jokeController.js
@@ -1,11 +1,13 @@
-const Joke = require('../models/joke');
+const Joke = require("../models/joke");
 
 const getAllJokes = async (req, res, next) => {
   try {
     const jokes = await Joke.findAll();
     res.status(200).json(jokes);
   } catch (err) {
-    res.status(500).json({ error: 'An error occured while retrieving all the jokes' });
+    res
+      .status(500)
+      .json({ error: "An error occured while retrieving all the jokes" });
   }
 };
 
@@ -14,17 +16,26 @@ const getOneJoke = async (req, res, next) => {
     const joke = await Joke.findByPk(req.params.id);
     res.status(200).json(joke);
   } catch (err) {
-    res.status(500).json({ error: `An error occured while retrieving a joke with id ${req.params.id}` });
+    res.status(500).json({
+      error: `An error occured while retrieving a joke with id ${req.params.id}`,
+    });
   }
 };
 
 // A MODIFIER :
 const getRandomJoke = async (req, res, next) => {
   try {
-    const joke = await Joke.findByPk(req.params.id);
-    res.status(200).json(joke);
+    // const count = await Joke.count();
+    // const randomIndex = Math.floor(Math.random() * count);
+    // const joke = await Joke.findAll({
+    //   offset: randomIndex,
+    //   limit: 1,
+    // });
+    // return res.status(200).json(joke);
   } catch (err) {
-    res.status(500).json({ error: `An error occured while retrieving a joke with id ${req.params.id}` });
+    res
+      .status(500)
+      .json({ error: "An error occured while retrieving a random joke" });
   }
 };
 
@@ -34,7 +45,9 @@ const createJoke = async (req, res, next) => {
     const newJoke = await Joke.create({ question, answer });
     res.status(201).json(newJoke);
   } catch (error) {
-    res.status(500).json({ error: 'An error occured while creating a new joke' });
+    res
+      .status(500)
+      .json({ error: "An error occured while creating a new joke" });
   }
 };
 
